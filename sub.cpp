b0178f21473c82f101734951cfb6cfd0bb6cd9ad
Anjali chopariya <134685296+anni2017@users.noreply.github.com>
2024-04-24 15:58:30 +0530

    Add files via upload

---
+++ sub.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sub; // sub ->subtraction
+    cout<<"enter first number : "<<endl;
+    cin>>a;
+    cout<<"enter second number : "<<endl;
+    cin>>b;
+    sub=a-b;
+    cout<<"sum of two number is : "<<sub<<endl;
+    
+    return 0;
+}
