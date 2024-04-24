0c03f074d4cb24ee8b3a284e127385e5a1abc164
anni2017 <anjalichopariya2008@gmail.com>
2024-04-24 14:56:34 +0530

    Add program to add 2 number

---
+++ Sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"enter first number : "<<endl;
+    cin>>a;
+    cout<<"enter second number : "<<endl;
+    cin>>b;
+    sum=a+b;
+    cout<<"sum of two number is : "<<sum<<endl;
+    
+    return 0;
+}
