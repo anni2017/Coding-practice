c70ae75f554e925082e61917aad64517fa223a5f
Anjali chopariya <134685296+anni2017@users.noreply.github.com>
2024-04-28 13:07:17 +0530

    Add the code of modulus operator

---
+++ mod(operator).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,mod; // return the remainder of the division of two numbers
+    cout<<"enter first number : "<<endl;
+    cin>>a;
+    cout<<"enter second number : "<<endl;
+    cin>>b;
+    mod=a%b;
+    cout<<"sum of two number is : "<<mod<<endl;
+
+    return 0;
+}
